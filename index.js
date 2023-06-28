0bb22189a8fcbf6b0464e3790cce3d12d4173f8f
Abed Nganga <ngangaabed@gmail.com>
2023-06-28 23:31:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = word.split('').reverse().join('')
+  return reversedWord === word;
 }
 
 /* 
   Add your pseudocode here
+
+  write a function called isPalindrome 
+  function receive one argument, a string
+  create reversed version of the string
+  if the string and reversed string are strictly equal return true
 */
 
 /*
   Add written explanation of your solution here
+  My solution contains a function that takes a strings and reverses the string to check if the string and reversed string read the same.It returns true is they read the same and false if they dont.
 */
 
 // You can run `node index.js` to view these console logs
